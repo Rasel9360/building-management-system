a30803d6c54360ea24a2e1373d0045ad314a2a37
Rasel9360 <raselmahamud9360@gmail.com>
2024-06-06 20:37:44 +0600

    axios interceptor setup

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/ferebase.config";
+import useAxiosPublic from "../hook/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 
@@ -8,6 +9,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -39,10 +41,23 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = auth.onAuthStateChanged(currentUser => {
             console.log("current User ", currentUser);
             setUser(currentUser);
-            setLoading(false);
-        })
+            if (currentUser) {
+                const userInfo = { email: currentUser?.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                            setLoading(false);
+                        }
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token');
+                setLoading(false);
+            }
+        });
         return () => unsubscribe;
-    }, [])
+    }, [axiosPublic])
 
     const authInfo = {
         user,
--- src/hook/useAxiosSecure.jsx
+++ src/hook/useAxiosSecure.jsx
@@ -1,9 +1,37 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    const { logOutUser } = useAuth();
+    const navigate = useNavigate();
+
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token');
+        // console.log(token);
+        config.headers.Authorization = `Bearer ${token}`
+        return config
+    }), function (error) {
+        return Promise.reject(error);
+    }
+
+    axiosSecure.interceptors.response.use(function (response) {
+        console.log();
+        return response
+    }, async (error) => {
+        const status = error.response.request.status
+        if (status === 401 || status === 403) {
+            await logOutUser();
+            navigate('/login')
+        }
+        console.log("error status in the interceptors", status);
+        return Promise.reject(error)
+    });
+
+
     return axiosSecure;
 };
 
